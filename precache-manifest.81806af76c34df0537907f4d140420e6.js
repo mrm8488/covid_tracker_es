5e357cc43c838cfb48d4534847b330d9c563d0e1
mrm8488 <mrm8488@gmail.com>
2020-06-04 19:46:41 +0200

    First commit

---
+++ precache-manifest.81806af76c34df0537907f4d140420e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f81714ad7e7516f978a55515d0b6539",
+    "url": "/index.html"
+  },
+  {
+    "revision": "391f6fef97dc06fcd7c9",
+    "url": "/static/css/main.8c0c72ed.chunk.css"
+  },
+  {
+    "revision": "7b42fa51aca7f9e75b77",
+    "url": "/static/js/2.31a0be0c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.31a0be0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391f6fef97dc06fcd7c9",
+    "url": "/static/js/main.aab9e3f7.chunk.js"
+  },
+  {
+    "revision": "f924a60bfc80cacc8da7",
+    "url": "/static/js/runtime-main.0349a059.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/covidLogo.d7265326.png"
+  }
+]);
